e0781a9ff61c7b3be233b0cabb6f08fb0d22b771
Aidan Brus <aidanbrus@gmail.com>
2025-08-08 15:46:20 -0600

    a0808-ThickTrackNotFlat

--- src/landing.js
+++ src/landing.js
@@ -1,7 +1,8 @@
 // All the code for make the animations and background design for the home page in js
-// main.js
-console.log("Landing.js loaded");
+
+// console.log("Landing.js loaded");
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 // Create scene
 const scene = new THREE.Scene();
@@ -14,113 +15,152 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 
+camera.position.x = 0;
+camera.position.y = 50;
+camera.position.z = 200;
+
 // Create renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-// Create cube
-// const geometry = new THREE.BoxGeometry();
-// const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-// const cube = new THREE.Mesh(geometry, material);
-// scene.add(cube);
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.target.set(0, 50, 0); // The center point
+controls.enableDamping = true; // Smooth movement
+controls.dampingFactor = 0.05;
 
+// creating an array of vectors for a smooth curve
+// CatmullRomCurve3
+// SplineCurve
+ const curve = new THREE.CatmullRomCurve3( [
+  new THREE.Vector3( 52, 5, 0 ), // almost actual start line 
+  new THREE.Vector3( -76, 5, 0 ), // turn 1 start
+  new THREE.Vector3( -78.5, 5, 0),
+  new THREE.Vector3( -78.5, 5.5, 0),
+  new THREE.Vector3( -79, 6, 0),
+  new THREE.Vector3( -79, 8, 0),
+  new THREE.Vector3( -78, 9, 0),
+  new THREE.Vector3( -76, 11, 0 ), // turn 1 end
+  new THREE.Vector3(  -66, 17.5, 0), // turn 1a start
+  new THREE.Vector3( -61, 19, 0),
+  new THREE.Vector3( -58, 20, 0), // turn 1a end
+  new THREE.Vector3( -10, 20, -5), // turn 2 start
+  new THREE.Vector3( -8, 21, -6),
+  new THREE.Vector3( -7, 22, -7),
+  new THREE.Vector3( -6, 25, -8),
+  new THREE.Vector3( -6.3, 27, -9),
+  new THREE.Vector3( -8, 29, -10),
+  new THREE.Vector3(-9, 29.5, -11), // turn 2 end
+  new THREE.Vector3( -31, 38.5, -12), // turn 3 start
+  new THREE.Vector3( -32.5, 40, -13.5),
+  new THREE.Vector3( -34, 43, -15), // turn 3 end
+  new THREE.Vector3( -44, 85, -15),
+  new THREE.Vector3( -45, 97.5, -12),
+  new THREE.Vector3( -45, 100, -10), // turn 4 start
+  new THREE.Vector3( -44.5, 107.5, -10),
+  new THREE.Vector3( -46, 110, -10),
+  new THREE.Vector3( -50, 114, -10), // turn 4 end
+  new THREE.Vector3( -60, 122, -8.5), // turn 5 start
+  new THREE.Vector3( -67, 130, -7),
+  new THREE.Vector3( -67.5, 133, -6.5),
+  new THREE.Vector3( -67, 136, -5.5), 
+  new THREE.Vector3( -63.5, 139, -4), 
+  new THREE.Vector3( -60, 140, -3), // turn 5 end
+  new THREE.Vector3( -11, 136.5, -2), // turn 6 start
+  new THREE.Vector3( -10, 136, -1.8), 
+  new THREE.Vector3( -10, 135, -1.6), // turn 6 end
+  new THREE.Vector3( -10, 129, -1.6), // turn 7 start
+  new THREE.Vector3( -9.5, 127.5, -1.4),
+  new THREE.Vector3( -8, 126, -1.2),// turn 7 end
+  new THREE.Vector3( 6, 116, -1), // turn 8 start
+  new THREE.Vector3( 10, 115, -0.8),
+  new THREE.Vector3( 14, 116, -0.6), // turn 8 end
+  new THREE.Vector3( 22, 122, -0.4), // turn 9 start
+  new THREE.Vector3( 25, 122.5, -0.2),
+  new THREE.Vector3( 28, 120, 0), // turn 9 end
+  new THREE.Vector3( 36, 110, 0), // turn 10 start
+  new THREE.Vector3( 41, 103, 0), 
+  new THREE.Vector3( 45, 101, 0), // turn 10 end
+  new THREE.Vector3( 57.5, 96, 0), // turn 11 start
+  new THREE.Vector3( 62, 93, 0),
+  new THREE.Vector3( 63, 90, 0), // turn 11 end
+  new THREE.Vector3( 60.5, 31, 0), /// turn 12 start
+  new THREE.Vector3( 60, 29, 0), 
+  new THREE.Vector3( 58.5, 28.5, 0), // turn 12 end
+  new THREE.Vector3( 37, 33, 0), // turn 12a
+  new THREE.Vector3( 29, 33, 0), // turn 13 start
+  new THREE.Vector3( 26, 32, 0),
+  new THREE.Vector3( 23.5, 28.5, 0),
+  new THREE.Vector3( 23, 26.5, 0),
+  new THREE.Vector3( 23.5, 24.5, 0),
+  new THREE.Vector3( 26, 21, 0),
+  new THREE.Vector3( 29, 20, 0), // turn 13 end
+  new THREE.Vector3( 52, 20, 0), // turn 14 start
+  new THREE.Vector3( 55, 19.3, 0),
+  new THREE.Vector3( 58, 17.1, 0),
+  new THREE.Vector3( 59.5, 14.4, 0),
+  new THREE.Vector3( 60, 12.5, 0),
+  new THREE.Vector3( 59.5, 10.5, 0),
+  new THREE.Vector3( 58, 7.9, 0),
+  new THREE.Vector3( 55, 5.7, 0),
+  new THREE.Vector3( 52, 5, 0), // turn 14 end
+  new THREE.Vector3( 51, 5, 0)
+ ] )
 
-// Animation loop
+const points = curve.getPoints( 100 );
+// const smoothCurve = new THREE.BufferGeometry().setFromPoints( points );
+
+// const material = new THREE.LineBasicMaterial( { color: 0xff0000 } );
+
+// Create the final object to add to the scene
+// const curveObject = new THREE.Line( geometry, material );
+// scene.add( curveObject);
+
+// extrude track from line
+// width->thick and vice versa
+const thickTrack = 4, widthTrack = 2;
+
+const shape = new THREE.Shape();
+shape.moveTo( -widthTrack/2,thickTrack/2 );
+shape.lineTo( -widthTrack/2, thickTrack/2 );
+shape.lineTo( widthTrack, thickTrack/2 );
+shape.lineTo( widthTrack/2, -thickTrack/2 );
+shape.lineTo( -widthTrack/2, -thickTrack/2 );
+
+const extrudeSettings = {
+	steps: 1000,
+	depth: 10,
+	bevelEnabled: false,
+	//bevelThickness: 1,
+	//bevelSize: 1,
+	//bevelOffset: 0,
+	//bevelSegments: 5,
+  extrudePath: curve
+};
+
+const geometry = new THREE.ExtrudeGeometry( shape, extrudeSettings );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const mesh = new THREE.Mesh( geometry, material ) ;
+scene.add( mesh );
+
+// Animation loop, spin
 //function animate() {
 //  requestAnimationFrame(animate);
 
-//  cube.rotation.x += 0.01;
-//  cube.rotation.y += 0.01;
+//  mesh.rotation.x += 0.01;
+//  mesh.rotation.y += 0.01;
 
 //  renderer.render(scene, camera);
 //}
 
-camera.position.x = 0;
-camera.position.y = 7;
-camera.position.z = 15;
+// renderer.render(scene,camera);
 
-const material = new THREE.LineBasicMaterial({
-	color: 0x0000ff
-});
-
-const points = [];
-points.push( new THREE.Vector3( 5.2, 0.5, 0 ) ); // almost star line actually
-points.push( new THREE.Vector3( -7.6, 0.5, 0 ) ); // turn 1 start
-points.push( new THREE.Vector3( -7.85, 0.55, 0) );
-points.push( new THREE.Vector3( -7.9, 0.6, 0) );
-points.push( new THREE.Vector3( -7.9, 0.8, 0) );
-points.push( new THREE.Vector3( -7.8, 0.9, 0) );
-points.push( new THREE.Vector3( -7.6, 1.1, 0 ) ); // turn 1 end
-points.push( new THREE.Vector3(  -6.6, 1.75, 0) ); // turn 1a start
-points.push( new THREE.Vector3( -6.1, 1.9, 0) ); 
-points.push( new THREE.Vector3( -5.8, 2, 0) ); // turn 1a end
-points.push( new THREE.Vector3( -1, 2, 0) ); // turn 2 start
-points.push( new THREE.Vector3( -0.8, 2.1, 0) );
-points.push( new THREE.Vector3( -0.7, 2.2, 0) );
-points.push( new THREE.Vector3( -0.6, 2.5, 0) );
-points.push( new THREE.Vector3( -0.63, 2.7, 0) );
-points.push( new THREE.Vector3( -0.8, 2.9, 0) );
-points.push( new THREE.Vector3(-0.9, 2.95, 0 ) ); // turn 2 end
-points.push( new THREE.Vector3( -3.1, 3.85, 0) ); // turn 3 start
-points.push( new THREE.Vector3( -3.25, 4, 0) );
-points.push( new THREE.Vector3( -3.4, 4.3, 0) ); // turn 3 end
-points.push( new THREE.Vector3( -4.4, 8.5, 0) );
-points.push( new THREE.Vector3( -4.5, 9.75, 0) );
-points.push( new THREE.Vector3( -4.5, 10, 0) ); // turn 4 start
-points.push( new THREE.Vector3( -4.45, 10.75, 0) );
-points.push( new THREE.Vector3( -4.6, 11, 0) );
-points.push( new THREE.Vector3( -5, 11.4, 0) ); // turn 4 end
-points.push( new THREE.Vector3( -6, 12.2, 0 ) ); // turn 5 start
-points.push( new THREE.Vector3( -6.7, 13, 0) ); 
-points.push( new THREE.Vector3( -6.75, 13.3, 0) );
-points.push( new THREE.Vector3( -6.7, 13.6, 0) );
-points.push( new THREE.Vector3( -6.35, 13.9, 0) );
-points.push( new THREE.Vector3( -6, 14, 0) ); // turn 5 end
-points.push( new THREE.Vector3( -1.1, 13.7, 0) ); // turn 6 start
-points.push( new THREE.Vector3( -1, 13.6, 0) );
-points.push( new THREE.Vector3( -1, 13.5, 0) ); // turn 6 end
-points.push( new THREE.Vector3( -1, 12.9, 0) ); // turn 7 start
-points.push( new THREE.Vector3( -0.95, 12.75, 0) ); 
-points.push( new THREE.Vector3( -0.8, 12.6, 0) );// turn 7 end
-points.push( new THREE.Vector3( 0.6, 11.6, 0) ); // turn 8 start
-points.push( new THREE.Vector3( 1, 11.5, 0) ); 
-points.push( new THREE.Vector3( 1.4, 11.6, 0) ); // turn 8 end
-points.push( new THREE.Vector3( 2.2, 12.2, 0) ); // turn 9 start
-points.push( new THREE.Vector3( 2.5, 12.25, 0) );
-points.push( new THREE.Vector3( 2.8, 12, 0) ); // turn 9 end
-points.push( new THREE.Vector3( 3.6, 11, 0) ); // turn 10 start
-points.push( new THREE.Vector3( 4.1, 10.3, 0) ); 
-points.push( new THREE.Vector3( 4.5, 10.1, 0) ); // turn 10 end
-points.push( new THREE.Vector3( 5.75, 9.7, 0) ); // turn 11 start
-points.push( new THREE.Vector3( 6.2, 9.3, 0) ); 
-points.push( new THREE.Vector3( 6.3, 9, 0) ); // turn 11 end
-points.push( new THREE.Vector3( 6.05, 3.1, 0) ); // turn 12 start
-points.push( new THREE.Vector3( 6, 2.9, 0) );
-points.push( new THREE.Vector3( 5.85, 2.85, 0) ); //turn 12 end
-points.push( new THREE.Vector3( 3.7, 3.3, 0) ); // turn 12a
-points.push( new THREE.Vector3( 2.9, 3.3, 0) ); // turn 13 start
-points.push( new THREE.Vector3( 2.6, 3.2, 0) );
-points.push( new THREE.Vector3( 2.35, 2.85, 0) );
-points.push( new THREE.Vector3( 2.3, 2.65, 0) ); 
-points.push( new THREE.Vector3( 2.35, 2.45, 0) );
-points.push( new THREE.Vector3( 2.6, 2.1, 0) );
-points.push( new THREE.Vector3( 2.9, 2, 0) ); // turn 13 end
-points.push( new THREE.Vector3( 5.2, 2, 0) ); // turn 14 start
-points.push( new THREE.Vector3( 5.5, 1.93, 0) );
-points.push( new THREE.Vector3( 5.8, 1.71, 0) );
-points.push( new THREE.Vector3( 5.95, 1.44, 0) );
-points.push( new THREE.Vector3( 6, 1.25, 0) );
-points.push( new THREE.Vector3( 5.95, 1.05, 0) );
-points.push( new THREE.Vector3( 5.8, 0.79, 0) );
-points.push( new THREE.Vector3( 5.5, 0.57, 0) );
-points.push( new THREE.Vector3( 5.2, 0.5, 0) ); // turn 14 end
-
-const geometry = new THREE.BufferGeometry().setFromPoints( points );
-
-const line = new THREE.Line( geometry, material );
-scene.add( line );
-
-renderer.render(scene,camera);
-
+// Render loop, orbit camera
+function animate() {
+    requestAnimationFrame(animate);
+    controls.update();
+    renderer.render(scene, camera);
+}
 animate();
+
+//animate();
